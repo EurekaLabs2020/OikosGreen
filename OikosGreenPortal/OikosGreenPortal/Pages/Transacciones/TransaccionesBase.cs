01707b0ecbf13065a71924237b9623472f1c5e9a
Alexander Miranda Osorio <amiranda.osorio@gmail.com>
2021-03-01 22:54:04 -0500

    .

--- OikosGreenPortal/OikosGreenPortal/Pages/Transacciones/TransaccionesBase.cs
+++ OikosGreenPortal/OikosGreenPortal/Pages/Transacciones/TransaccionesBase.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,12 @@ namespace OikosGreenPortal.Pages.Transacciones
 {
     public class TransaccionesBase : ComponentBase
     {
+        [Inject] public IModalService _modal { get; set; }
+        [Inject] public ProtectedSessionStorage _storage { get; set; }
+
+        public List<>
+
+
+
     }
 }
