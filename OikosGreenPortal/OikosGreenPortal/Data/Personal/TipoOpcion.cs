c0b7ae9694bb5b8ac9bd6dc8c9e755b771e49f0b
Christian Camilo Orozco Osorio <camiloosorio96@outlook.com>
2021-03-01 21:20:18 -0500

    Opcion Razon

---
+++ OikosGreenPortal/OikosGreenPortal/Data/Personal/TipoOpcion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OikosGreenPortal.Data.Personal
+{
+    public class TipoOpcion
+    {
+        public TipoOpcion()
+        {
+        }
+        public List<String> tiposOpciones()
+        {
+            List<String> retorno = new List<String>()
+            { "MENU", "OPCION"};
+            return retorno;
+        }
+    }
+}
