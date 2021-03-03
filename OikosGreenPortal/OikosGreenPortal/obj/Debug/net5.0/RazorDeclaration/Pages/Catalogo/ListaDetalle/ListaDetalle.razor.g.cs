171bccc453a0fe4b45c95b5028dfe7182d3eaa24
Wilder Valencia <wilder.valencia@evedisa.com.co>
2021-03-03 09:26:21 -0500

    Agregar Titulo ListaDetalle WVZ

--- OikosGreenPortal/OikosGreenPortal/obj/Debug/net5.0/RazorDeclaration/Pages/Catalogo/ListaDetalle/ListaDetalle.razor.g.cs
+++ OikosGreenPortal/OikosGreenPortal/obj/Debug/net5.0/RazorDeclaration/Pages/Catalogo/ListaDetalle/ListaDetalle.razor.g.cs
@@ -166,6 +166,13 @@ using OikosGreenPortal.Data.Request;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 4 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\Pages\Catalogo\ListaDetalle\ListaDetalle.razor"
+           [Authorize]
+
+#line default
+#line hidden
+#nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/catalogo/listadetalle")]
     public partial class ListaDetalle : ListaDetalleBase
     {
