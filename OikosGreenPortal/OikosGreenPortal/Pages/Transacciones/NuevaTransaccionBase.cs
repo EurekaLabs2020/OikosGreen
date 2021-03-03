4f10c7adf69b24e0cd394b4225706bca0dff58bf
Alexander Miranda Osorio <amiranda.osorio@gmail.com>
2021-03-02 22:47:08 -0500

    .

--- OikosGreenPortal/OikosGreenPortal/Pages/Transacciones/NuevaTransaccionBase.cs
+++ OikosGreenPortal/OikosGreenPortal/Pages/Transacciones/NuevaTransaccionBase.cs
@@ -23,10 +23,17 @@ namespace OikosGreenPortal.Pages.Transacciones
         public Int64 _datoTipo { get; set; }
         public Int64 _datoClase { get; set; }
         public DateTime? _datoFecha { get; set; }
+        public Int64 _datoBodegaOrig { get; set; }
+        public Int64 _datoBodegadest { get; set; }
+        public Int64 _datoProveedor { get; set; }
 
 
         public List<Documento_data> _listaTipo { get; set; }
         public List<String> _tipo { get; set; }
+        public List<String> _tipoProv { get; set; }
+        public List<Bodega_data> _listaOrig { get; set; }
+        public List<Bodega_data> _listaDest { get; set; }
+        public List<TerceroTipo_data> _listaProv { get; set; }
         public String _Mensaje { get; set; }
         public String _mensajeIsDanger { get; set; }
 
@@ -43,7 +50,8 @@ namespace OikosGreenPortal.Pages.Transacciones
 
             TipoDocumento tipo = new TipoDocumento();
             _tipo = tipo.tiposDocumentos();
-
+            TipoTerceroTipo tipoUbica = new TipoTerceroTipo();
+            _tipoProv = tipoUbica.tiposTerceroTipo();
             DocumentosRequest _dataRequest = new DocumentosRequest();
             try
             {
@@ -58,6 +66,16 @@ namespace OikosGreenPortal.Pages.Transacciones
                 _dataRequest = JsonConvert.DeserializeObject<DocumentosRequest>(resultado.Content.ReadAsStringAsync().Result.ToString());
                 if (_dataRequest != null && _dataRequest.entities != null && _dataRequest.entities.Count > 0)
                     _listaTipo = _dataRequest.entities.Where(w=>w.type==_tipo[1]).ToList();
+                //Obtiene la bodega
+                var resultadoBodega = await General.solicitudUrl<String>(_dataStorage.user.token, "GET", Urls.urlbodega_getall, "");
+                BodegasRequest _dataRequestBodega = JsonConvert.DeserializeObject<BodegasRequest>(resultadoBodega.Content.ReadAsStringAsync().Result.ToString());
+                if (_dataRequestBodega != null && _dataRequestBodega.entities != null && _dataRequestBodega.entities.Count > 0)
+                    _listaOrig = _listaDest = _dataRequestBodega.entities;
+                //Obtiene Proveedor
+                var resultadoProveedor = await General.solicitudUrl<String>(_dataStorage.user.token, "GET", Urls.urlterceropunto_getall, "");
+                TercerosTipoRequest _dataRequestProveedor = JsonConvert.DeserializeObject<TercerosTipoRequest>(resultadoProveedor.Content.ReadAsStringAsync().Result.ToString());
+                if (_dataRequestProveedor != null && _dataRequestProveedor.entities != null && _dataRequestProveedor.entities.Count > 0)
+                    _listaProv = _dataRequestProveedor.entities.Where(w=>w.type== _tipoProv[0]).ToList();
             }
             catch (Exception ex)
             {
@@ -73,6 +91,10 @@ namespace OikosGreenPortal.Pages.Transacciones
                 _resumenDetalle += _listaTipo.Where(w => w.id == _datoTipo).Select(s=>s.name).FirstOrDefault();
         }
 
+        public void OnDateChanged(DateTime? date)
+        {
+            _datoFecha = date;
+        }
 
 
     }
