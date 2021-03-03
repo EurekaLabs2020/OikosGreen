018e8dca31b076806eddf5b807c21627ee52a1cc
Christian Camilo Orozco Osorio <camiloosorio96@outlook.com>
2021-03-03 18:19:20 -0500

    Camilo Cambio de clave

---
+++ OikosGreenPortal/OikosGreenPortal/Pages/Auth/Cambiar Clave/NuevaClaveIndexBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazored.Modal;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Newtonsoft.Json;
+using OikosGreenPortal.Data.Personal;
+using OikosGreenPortal.Data.Request;
+using OikosGreenPortal.Data.Send;
+using OikosGreenPortal.PersonalClass;
+
+namespace OikosGreenPortal.Pages.Auth.Cambiar_Clave
+{
+    public class NuevaClaveIndexBase : ComponentBase
+    {
+        [CascadingParameter] public BlazoredModalInstance BlazoredModal { get; set; }
+        [Parameter] public infoBrowser userLogueado { get; set; }
+        public ChangePassword _usuario { get; set; }
+        public String Mensaje { get; set; } = "";
+        public Boolean _mostrarPassword { get; set; } = true;
+        public Boolean _mostrarPassword2 { get; set; } = true;
+        public Boolean _mostrarPassword3 { get; set; } = true;
+        [Inject] public ProtectedSessionStorage _sessionStorage { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            _usuario = new ChangePassword();
+            await Cargue();
+        }
+        private async Task Cargue()
+        {
+            await Task.Delay(1);
+        }
+        public async Task togglePassTMP()
+        {
+            _mostrarPassword = !_mostrarPassword;
+        }
+
+        public async Task togglePass()
+        {
+            _mostrarPassword2 = !_mostrarPassword2;
+        }
+
+        public async Task toggleConfirmPass()
+        {
+            _mostrarPassword3 = !_mostrarPassword3;
+        }
+
+        public async Task changePass()
+        {
+            if (_usuario.password == _usuario.passwordconfirm)
+            {
+                String statusApi = "";
+                try
+                {
+                    ChangePassword changePass = new ChangePassword();
+                    _usuario.user = userLogueado.user.user;
+                    var resultado = await General.solicitudUrl<ChangePassword>(userLogueado.user.token, "POST", Urls.urlchange_Pass, _usuario);
+                    ResponsRequestBoolean respAuth = JsonConvert.DeserializeObject<ResponsRequestBoolean>(resultado.Content.ReadAsStringAsync().Result.ToString());
+                    if (respAuth != null && respAuth.status != null && respAuth.status.code == 200)
+                    {
+                        Mensaje = "Contraseña cambiada con éxito!!!";
+                        StateHasChanged();
+                    }
+                    else if (!String.IsNullOrEmpty(statusApi))
+                        Mensaje = "ERROR " + statusApi;
+                    else
+                        Mensaje = "ERROR " + respAuth.status.message + " VALIDE SU CONTRASEÑA ACTUAL";
+                }
+                catch (Exception ex)
+                {
+                    Mensaje = "ERROR CONSULTANDO DATOS" + ex.Message;
+                }
+            }
+            else
+            {
+                Mensaje = "CONTRASEÑAS NO COINCIDEN";
+
+            }
+
+        }
+
+
+
+    }
+}
--- OikosGreenPortal/OikosGreenPortal/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ OikosGreenPortal/OikosGreenPortal/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,154 +13,154 @@ namespace OikosGreenPortal.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 1 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 2 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 3 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 4 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 5 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 6 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 7 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 8 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 9 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 10 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 13 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 14 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Blazored.Menu;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 15 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Blazored.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 16 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Blazored.Modal.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 17 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 17 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 18 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 18 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using Blazorise.DataGrid;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 22 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 22 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.Pages.Auth.login;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 23 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 23 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.Pages.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 26 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 26 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.PersonalClass;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 27 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 27 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.Data.Personal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 28 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 28 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.Helpers;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 29 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
+#line 29 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\_Imports.razor"
 using OikosGreenPortal.Data.Request;
 
 #line default
@@ -174,7 +174,7 @@ using OikosGreenPortal.Data.Request;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 46 "E:\EK\OikosGreen\OikosGreenPortal\OikosGreenPortal\Shared\NavMenu.razor"
+#line 46 "C:\Desarrollos\OikosGreen S.A\OikosGreen_Web\OikosGreenPortal\OikosGreenPortal\Shared\NavMenu.razor"
        
 
     private bool collapseNavMenu = true;
