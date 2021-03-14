464edd52864f15fc273104669dd5e51d8fb4750a
Jhonatan Hurtado Herrera <jhonatan.hurtado@outlook.es>
2021-03-13 21:30:28 -0500

    JH

---
+++ OikosGreenPortal/OikosGreenPortal/Pages/Domicilios/Despachos/DespachosIndexBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OikosGreenPortal.Pages.Domicilios.Despachos
+{
+    public class DespachosIndexBase
+    {
+    }
+}
---
+++ OikosGreenPortal/OikosGreenPortal/Pages/Domicilios/Entregas/EntregasIndexBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OikosGreenPortal.Pages.Domicilios.Entregas
+{
+    public class EntregasIndexBase
+    {
+    }
+}
